39fd4f981908472b03a8f20786c13a83c54c4c19
Joe P Shoulak <joe@squarecompass.io>
2022-10-14 20:53:50 -0500

    all base functionality implemented

--- db/seed.sql
+++ db/seed.sql
@@ -1,57 +1,39 @@
 /* ==== SEED FILE ==== */
+USE employee_tracker;
 
-/* == DEPARTMENT == */
-DROP TABLE department;
-CREATE TABLE department (
-    id int primary key,
-    name VARCHAR(30)
-);
+INSERT INTO department
+(name)
+VALUES
+("Technology"),
+("Operations"),
+("Human Resources"),
+("Finance");
 
-INSERT INTO department (id, name) VALUES (1, "Technology");
-INSERT INTO department (id, name) VALUES (2, "Operations");
-INSERT INTO department (id, name) VALUES (3, "Human Resources");
-INSERT INTO department (id, name) VALUES (4, "Finance");
+INSERT INTO role 
+(title, salary, department_id)
+VALUES
+("Manager", 50000, 1),
+("Manager", 50000, 2),
+("Manager", 50000, 3),
+("Manager", 50000, 4),
+("Employee", 25000, 1),
+("Employee", 25000, 2),
+("Employee", 25000, 3),
+("Employee", 25000, 4);
 
-/* == ROLE == */
-DROP TABLE role;
-CREATE TABLE role (
-    id int primary key,
-    title VARCHAR(30),
-    salary DECIMAL,
-    department_id INT
-);
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id)
+VALUES
+("Joe", "Shoulak", 1, null),
+("Judy", "Shoulak", 2, null),
+("Sarah", "Shoulak", 3, null),
+("Jake", "Shoulak", 4, null),
 
-INSERT INTO role (id, title, salary, department_id) VALUES (1, "Manager", 50000, 1);
-INSERT INTO role (id, title, salary, department_id) VALUES (2, "Manager", 50000, 2);
-INSERT INTO role (id, title, salary, department_id) VALUES (3, "Manager", 50000, 3);
-INSERT INTO role (id, title, salary, department_id) VALUES (4, "Manager", 50000, 4);
-INSERT INTO role (id, title, salary, department_id) VALUES (5, "Employee", 25000, 1);
-INSERT INTO role (id, title, salary, department_id) VALUES (6, "Employee", 25000, 2);
-INSERT INTO role (id, title, salary, department_id) VALUES (7, "Employee", 25000, 3);
-INSERT INTO role (id, title, salary, department_id) VALUES (8, "Employee", 25000, 4);
-
-/* == EMPLOYEE == */
-DROP TABLE employee;
-CREATE TABLE employee (
-    id int primary key,
-    first_name VARCHAR(30),
-    last_name VARCHAR(30),
-    role_id INT,
-    manager_id INT
-);
-
-/* Managers */
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (1, "Joe", "Shoulak", 1, null);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (2, "Judy", "Shoulak", 2, null);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (3, "Sarah", "Shoulak", 3, null);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (4, "Jake", "Shoulak", 4, null);
-
-/* Employees */
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (5, "Alice", "Armstrong", 5, 1);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (6, "Bob", "Barker", 5, 1);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (7, "Chloe", "Clinton", 6, 2);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (8, "Don", "Davidson", 6, 2);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (9, "Ed", "Eckerson", 7, 3);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (10, "Frank", "Fitzwater", 7, 3);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (11, "Geroge", "van Geoth", 8, 4);
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id) VALUES (12, "Harry", "Hayes", 8, 4);
+("Alice", "Armstrong", 5, 1),
+("Bob", "Barker", 5, 1),
+("Chloe", "Clinton", 6, 2),
+("Don", "Davidson", 6, 2),
+("Ed", "Eckerson", 7, 3),
+("Frank", "Fitzwater", 7, 3),
+("Geroge", "van Geoth", 8, 4),
+("Harry", "Hayes", 8, 4);
--- index.js
+++ index.js
@@ -6,21 +6,6 @@ import Questions from "./lib/questions.js";
 const prompt = inquirer.prompt;
 const sql = new JoeSQL();
 
-/* == HELPERS == */
-// Add a new object to the related table
-const addNew = async (table) => {
-    // Get the data from the user via Inquirer
-    let objectData = await prompt(Questions[table]);
-
-    // Update the object with a new ID that isn't in use
-    objectData.id = (await sql.maxID(table)) + 1;
-    console.log(objectData);
-
-    // Insert the object into the database
-    sql.insert(table, objectData);
-}
-
-// TODO: Rename this function
 // Perform the "main menu" query to identify the chosen user action
 const performAction = async (action) => {
     switch (action) {
@@ -34,16 +19,23 @@ const performAction = async (action) => {
             sql.displayTable("employee");
             break;
         case "Add a Department":
-            await addNew("department");
+            sql.insert("department", await prompt(Questions.newDepartment));
             break;
         case "Add a Role":
-            await addNew("role");
+            sql.insert("role", await prompt(Questions.newRole));
             break;
         case "Add an Employee":
-            await addNew("employee");
+            sql.insert("employee", await prompt(Questions.newEmployee));
             break;
         case "Update an Employee Role":
-            console.log("Not yet implemented..."); // TODO: Implement this
+            let updateInfo = await prompt(Questions.updateEmployee);
+
+            let change = {
+                column: "role_id",
+                value: updateInfo.role
+            };
+
+            sql.update("employee", updateInfo.id, change);
             break;
         default:
             break;
@@ -55,7 +47,7 @@ const init = async () => {
     let action;
 
     while (action != "Exit") {
-        action = (await prompt(Questions.main)).action;
+        action = (await prompt(Questions.mainAction)).action;
         await performAction(action);
     }
 }
